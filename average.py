0b48e91f65e9012507deff26d663ba995af3c26b
Keddins <eddinskeathan@gmail.com>
2023-10-11 20:34:17 -0700

    average.py

---
+++ average.py
@@ -0,0 +1,14 @@
+def main():
+    print("1,2,3,4,5")
+    num1 = float(input(1))
+    num2 = float(input(2))
+    num3 = float(input(3))
+    num4 = float(input(4))
+    num5 = float(input(5))
+
+    average = (num1 + num2 + num3 + num4 + num5) / 5
+
+    print("The average of those numbers is: " + str(average))
+
+if __name__ == "__main__":
+    main()
